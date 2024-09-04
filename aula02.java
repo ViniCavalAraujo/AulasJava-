5ca9c35322e2426895be9a908c894617b896d745
ViniCavalAraujo <149206889+ViniCavalAraujo@users.noreply.github.com>
2024-09-04 19:09:49 +0000

    Aula finalizada

---
+++ aula02.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class aula02{
+
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite seu nome :");
+        String nome = sc.nextLine();
+        System.out.println(nome);
+
+
+        sc.close(); //se nao fechar ainda fica em uso da memória
+
+
+
+
+
+    }
+
+}
